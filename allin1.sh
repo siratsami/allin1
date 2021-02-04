dcd3054a8832dcba0da9a15c969a8ff9f13fbb7c
siratsami <60298069+siratsami@users.noreply.github.com>
2021-02-04 23:55:58 +0300

    Add files via upload

---
+++ allin1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo " -------------------------------"
+echo " | ALLIN1 full automated recon |"
+echo " |       YouTube: B4GG3R       |"
+echo " -------------------------------"
+echo ""
+echo " --- Finding subdomains for $1 ---"
+echo ""
+echo " --- Using crt.sh ---"
+curl -s https://crt.sh/?q=%25.$1 | grep "$1" | grep "<TD>" | cut -d">" -f2 | cut -d"<" -f1 | sed s/*.//g | sort -u | tee subs/$1_crtsh
+echo ""
+echo " --- Using assetfinder ---"
+assetfinder --subs-only $1 | grep $1 | sort -u | tee subs/$1_assetfinder
+echo ""
+echo " --- Using subfinder ---"
+subfinder -all -d $1 -nC -nW -silent | sort -u | tee subs/$1_subfinder
+cat subs/* | sort -u >> subs/$1_allsubs
+echo ""
+echo " --- Collecting $1 subdomain ip addresses ---"
+for sub in $(cat subs/$1_allsubs); do echo "Subdomain: $sub" && dig +short $sub | tail -n1 ; done | tee dig/$1_subips
+echo ""
+echo " --- Trying whois for $1 ip addresses ---"
+for ip in $(cat dig/$1_subips | grep -v S | sort -u); do whois -h whois.cymru.com " -v $ip"; done | tee whois/$1_ipswhois
+echo ""
+echo " --- Cleaning $1 ip addresses from cloudflare & incapsula ---"
+cat whois/$1_ipswhois |  grep -v -e CLOUDFLARE -e INCAPSULA -e "| IP" | awk '{print $3}' | tee nocf/$1_nocfips
+echo ""
+echo " --- Scaning $1 ip addresses for top ports with nmap ---"
+for ip in $(cat nocf/$1_nocfips); do nmap $ip -T5; done | tee nmap/$1_iports
+echo ""
+echo " --- Using httprobe to resolve browsable $1 subdomains ---"
+cat subs/$1_allsubs | httprobe | tee httprobe/$1_https
+echo ""
+echo " --- Using curl to collecting $1 httprobed responses ---"
+for httpr in $(cat httprobe/$1_https); do curl $httpr -I --max-time 20
+echo "URL: $httpr" | tee curl/$1_curls; done
+echo ""
+echo " --- Using wayback machine for $1 ---"
+waybackurls $1 >> wayback/$1_wayback
+echo " *Saved in wayback/$1_wayback"
+echo ""
+echo " --- Using curl to find origin ip with virtual host ---"
+for server in $(cat httprobe/$1_https); do curl $server -H "Host: $1" -I | grep -e "HTTP/" -e "Content-Length" | tee virtualh/$1_virtuals
+echo "Server: $server";done
+echo ""
+echo " --- Done ---"
---
+++ install.sh
@@ -0,0 +1,32 @@
+!#/bin/bash
+
+echo " ----------------------------------------"
+echo " | Installing required tools for allin1 |"
+echo " | Please run this script as sudo       |"
+echo " ----------------------------------------"
+echo ""
+echo " --- Craeting directorys ---"
+mkdir curl dig httprobe nmap nocf subs virtualh wayback whois
+echo ""
+echo " --- Installing nmap ---"
+apt install nmap
+echo ""
+echo " --- Installing golang ---"
+apt install golang
+echo ""
+echo " --- Installing assetfinder ---"
+go get -u github.com/tomnomnom/assetfinder
+cp -r ~/go/bin/assetfinder /usr/bin/assetfinder
+echo ""
+echo " --- Installing subfinder ---"
+GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder
+cp -r ~/go/bin/subfinder /usr/bin/subfinder
+echo ""
+echo " --- Installing httprobe ---"
+go get -u github.com/tomnomnom/httprobe
+echo ""
+echo " --- Installing waybackurls ---"
+go get github.com/tomnomnom/waybackurls
+sudo cp -r ~/go/bin/waybackurls /usr/bin/waybackurls
+echo ""
+echo " Done"
