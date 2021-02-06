a84000d3dcde3028fc81a7a4c48fbdcbb05046fd
siratsami <60298069+siratsami@users.noreply.github.com>
2021-02-06 14:04:35 +0300

    Update allin1.sh

--- allin1.sh
+++ allin1.sh
@@ -7,6 +7,9 @@ echo " -------------------------------"
 echo ""
 echo " --- Finding subdomains for $1 ---"
 echo ""
+echo " --- Using Owasp Amass ---"
+amass enum -d $1 -o subs/$1_amass
+echo ""
 echo " --- Using crt.sh ---"
 curl -s https://crt.sh/?q=%25.$1 | grep "$1" | grep "<TD>" | cut -d">" -f2 | cut -d"<" -f1 | sed s/*.//g | sort -u | tee subs/$1_crtsh
 echo ""
@@ -20,13 +23,13 @@ echo ""
 echo " --- Collecting $1 subdomain ip addresses ---"
 for sub in $(cat subs/$1_allsubs); do echo "Subdomain: $sub" && dig +short $sub | tail -n1 ; done | tee dig/$1_subips
 echo ""
-echo " --- Trying whois for $1 ip addresses ---"
+echo " --- Trying whois for $1 ip adresses ---"
 for ip in $(cat dig/$1_subips | grep -v S | sort -u); do whois -h whois.cymru.com " -v $ip"; done | tee whois/$1_ipswhois
 echo ""
-echo " --- Cleaning $1 ip addresses from cloudflare & incapsula ---"
+echo " --- Cleaning $1 ip adresses from cloudflare & incapsula ---"
 cat whois/$1_ipswhois |  grep -v -e CLOUDFLARE -e INCAPSULA -e "| IP" | awk '{print $3}' | tee nocf/$1_nocfips
 echo ""
-echo " --- Scaning $1 ip addresses for top ports with nmap ---"
+echo " --- Scaning $1 ip adresses for top ports with nmap ---"
 for ip in $(cat nocf/$1_nocfips); do nmap $ip -T5; done | tee nmap/$1_iports
 echo ""
 echo " --- Using httprobe to resolve browsable $1 subdomains ---"
@@ -34,14 +37,14 @@ cat subs/$1_allsubs | httprobe | tee httprobe/$1_https
 echo ""
 echo " --- Using curl to collecting $1 httprobed responses ---"
 for httpr in $(cat httprobe/$1_https); do curl $httpr -I --max-time 20
-echo "URL: $httpr" | tee curl/$1_curls; done
+echo "URL: $httpr"; done | tee curl/$1_curls
 echo ""
 echo " --- Using wayback machine for $1 ---"
 waybackurls $1 >> wayback/$1_wayback
 echo " *Saved in wayback/$1_wayback"
 echo ""
 echo " --- Using curl to find origin ip with virtual host ---"
-for server in $(cat httprobe/$1_https); do curl $server -H "Host: $1" -I | grep -e "HTTP/" -e "Content-Length" | tee virtualh/$1_virtuals
-echo "Server: $server";done
+for server in $(cat httprobe/$1_https); do curl $server -H "Host: $1" -I | grep -e "HTTP/" -e "Content-Length"
+echo "Server: $server";done | tee virtualh/$1_virtuals
 echo ""
 echo " --- Done ---"
